bfade06f9283b41ec45ed1f8f06af3b310d69cf2
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-01-17 13:19:51 -0500

    Se añade validacion de conexion a la BD

--- back/index.js
+++ back/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import database from './database/database.js';
 
 const app = express();
 const port = 3100;
+
+try {
+    await database.authenticate()
+    console.log(`Servidor corriendo`)
+} catch (error) {
+    console.log(`Servidor corriendo: ${error}`)
+}
 
 app.listen(port, ()=>{
     console.log('Servidor corriendo en el puerto' + port)
