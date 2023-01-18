ccc23f963ce022a4854a876612172b79f0209c32
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-01-17 23:33:06 -0500

    Las imagenes de las marcas se colocan como enlace para que la respectiva imagen lleve a su pagina

--- front/src/components/helpers/imgs/ContHeader.jsx
+++ front/src/components/helpers/imgs/ContHeader.jsx
@@ -9,15 +9,15 @@ const ContHeader = () => {
     return (
         <main className='contHeader'>
             <section className='sectionImgsLinea'>
-                <img src={americanino} alt="" />
-                <img src={americanEagle} alt="" />
-                <img src={chevignon} alt="" />
+                <a target={'_blank'} href="https://www.americanino.com/"> <img src={americanino} alt="" /> </a>
+                <a target={'_blank'} href="https://www.ae.com.co/"> <img src={americanEagle} alt="" /> </a>
+                <a target={'_blank'} href="https://www.chevignon.com.co/"> <img src={chevignon} alt="" /> </a>
             </section>
-            <h1>Grupo Uribe</h1>
+            <a target={'_blank'} href="https://www.johnuribe.com.co/"> <h1 className='title'>Grupo Uribe</h1> </a>
             <section className='sectionImgsLinea'>
-                <img src={esprit} alt="" />
-                <img src={nafnaf} alt="" />
-                <img src={rifle} alt="" />
+                <a target={'_blank'} href="https://www.esprit.com.co/"> <img src={esprit} alt="" /> </a>
+                <a target={'_blank'} href="https://www.nafnaf.com.co/"> <img src={nafnaf} alt="" /> </a>
+                <a target={'_blank'} href="https://www.rifle.com.co/"> <img src={rifle} alt="" /> </a>
             </section>
         </main>
     )
